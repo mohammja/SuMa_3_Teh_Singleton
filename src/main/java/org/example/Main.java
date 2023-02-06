a497f03068c28294045f889f268cc7e007bbcadf
mohammja <mohammja@metropolia.fi>
2023-02-06 22:41:32 +0200

    Done!

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Logger logger = Logger.getInstance();
+        logger.log("First message");
+        logger.log("Second message");
+    }
+}
